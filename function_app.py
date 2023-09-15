99baf979d026c3faed1e0c7e2da567ab3d8222b2
Peter Nociar <peter.n@elsewhen.com>
2023-09-15 11:45:27 +0100

    test

---
+++ function_app.py
@@ -0,0 +1,22 @@
+import pydevd_pycharm
+pydevd_pycharm.settrace('localhost', port=9000, stdoutToServer=True, stderrToServer=True)
+
+import azure.functions as func
+import logging
+
+
+app = func.FunctionApp()
+
+
+@app.function_name(name="hello_world")
+@app.route(route="hello", methods=["POST"])
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
+    input = req.get_json()
+    name = input.get("name", "Anonymous")
+    return func.HttpResponse(f"Hello {name}")
+
+
+@app.function_name(name="hello_world_2")
+@app.route(route="hi")
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse("Hi there")
